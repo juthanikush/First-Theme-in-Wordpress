baeb721980038a2c1e2ad6a5f24e4c619905eff1
KUSH  JUTHNAI <81456337+juthanikush@users.noreply.github.com>
2023-08-05 15:14:35 +0530

    mandetory changes

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="container-fluid footer_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 col-md-9 mx-auto">
+          <p>
+            &copy; 2019 All Rights Reserved By
+            <a href="https://html.design/">Free Html Templates</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer section -->
+
+
+  <script src="<?php bloginfo('template_directory'); ?>/js/jquery-3.4.1.min.js"></script>
+  <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.js"></script>
+
+</body>
+
+</html>
